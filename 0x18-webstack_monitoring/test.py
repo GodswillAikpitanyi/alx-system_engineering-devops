3c02acf5d1bc23b3c267c6ea3414f0d2b31b6bc7
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-05-15 11:19:53 +0000

    test file correction

--- 0x18-webstack_monitoring/test.py
+++ 0x18-webstack_monitoring/test.py
@@ -3,10 +3,10 @@ import requests
 
 # Set up the API endpoint and parameters
 api_url = "https://api.datadoghq.com/api/v1/dashboard/"
-api_key = "81ed10735392a0c2e87f1e45964539d6"
-api_app_key = "4b405bb4b4e9f21d3bbd9ea5b952041a811ac594"
+api_key = "<api_key>"
+api_app_key = "<application_key>"
 headers = {'Content-type': 'application/json', 'DD-API-KEY': api_key, 'DD-APPLICATION-KEY': api_app_key}
-query_params = {'filter': 'host:153520-web-01'}
+query_params = {'filter': 'host:<student_id-web-01>'}
 
 # Make the API call to get the host details
 response = requests.get(api_url, headers=headers, params=query_params)
